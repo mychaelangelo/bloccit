d4e2cb2cb2a3cc2a5d2a66a6d2faf7a56ad56b84
michael <michael.tefula@gmail.com>
2014-05-17 12:13:30 +0000

    Associated users and posts

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,8 +1,22 @@
 require 'faker'
 
+# Create Users
+5.times do
+  user = User.new(
+    name: Faker::Name.name,
+    email: Faker::Internet.email,
+    password: Faker::Lorem.characters(10)
+    )
+  user.skip_confirmation!
+  user.save
+end
+users = User.all
+
+
 # Create Posts
 50.times do
   Post.create(
+    user: users.sample,
     title: Faker::Lorem.sentence,
     body: Faker::Lorem.paragraph
   )
@@ -18,5 +32,6 @@ posts = Post.all
 end
 
 puts "Seed finished"
+puts "#{User.count} users created"
 puts "#{Post.count} posts created"
 puts "#{Comment.count} comments created"
