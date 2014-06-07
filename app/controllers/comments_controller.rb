3b22ed8ba56e6723b07255eacf5950bbb70ef7a8
michael <michael.tefula@gmail.com>
2014-06-07 18:36:26 +0000

    Ajaxified comment destruction

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,5 +1,8 @@
 class CommentsController < ApplicationController
 
+  # enables the controller to respond to both HTML and JavaScript
+  respond_to :html, :js
+
   # This method simply instantiates an empty instance of Comment
   def new
     @post = Post.find(params[:post_id])
@@ -35,14 +38,16 @@ class CommentsController < ApplicationController
     @post = @topic.posts.find(params[:post_id])
 
     @comment = @post.comments.find(params[:id])
-
     authorize @comment
+
     if @comment.destroy
       flash[:notice] = "Comment was removed."
-      redirect_to [@topic, @post]
     else
       flash[:error] = "Comment couldn't be deleted. Try again."
-      redirect_to [@topic, @post]
+    end
+
+    respond_with(@comment) do |f|
+      f.html { redirect_to [@topic, @post] }
     end
   end
 
